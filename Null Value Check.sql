b32d6e5c4a00cc4b616979cd161365938ee08399
Divyanand14 <135130277+Divyanand14@users.noreply.github.com>
2024-09-10 23:22:42 +1000

    Add files via upload

---
+++ Null Value Check.sql
@@ -0,0 +1,8 @@
+select *from `ECommerce.customer_data`
+where Email is NULL or `Customer ID` is NULL
+
+select *from `ECommerce.order_data`
+where 'Order ID' is null
+
+select *from `ECommerce.order_details`
+'Product ID' is null
